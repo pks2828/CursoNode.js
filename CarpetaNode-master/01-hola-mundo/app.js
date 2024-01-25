7e39d4e12ee07eaa75214a4e3577655731a0e9ba
Angel Doria <danieldoriamoncada28@gmail.com>
2024-01-25 11:28:56 -0600

    Eliminada la carpeta

--- CarpetaNode-master/01-hola-mundo/app.js
+++
@@ -1,9 +0,0 @@
-console.log("Practica de programa");
-
-const multiplicar = ((num1,num2) =>{
-    return num1 * num2
-})
-
-
-console.log(`El resultado de la multiplicacion es ${multiplicar(4,2)}`);
-
